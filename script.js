ce79a56fcebf03a7a91d8639ed28d9a0441d88f7
LeandreQueille <113764950+LeandreQueille@users.noreply.github.com>
2022-12-18 19:11:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log(document)
+
+const body = document.querySelector('body')
+const button1 = document.getElementById("nightmode")
+const button2 = document.getElementById("lightmode")
+
+button1.addEventListener('click', function (body) {
+    document.body.style.backgroundColor = 'black'
+}
+)
+
+button2.addEventListener('click', function (body) {
+    document.body.style.backgroundColor = 'blanchedalmond'
+}
+)
+
+
